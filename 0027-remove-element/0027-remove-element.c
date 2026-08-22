270ae7e2855f90400a097fc1f2b8e5926430578d
Urvashi Zinzuvadiya <urvashi7211@gmail.com>
2026-08-22 15:22:09 +0530

    Time: 0 ms (100%), Space: 10.4 MB (29.18%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.c
@@ -0,0 +1,12 @@
+int removeElement(int* nums, int numsSize, int val) {
+    for(int i=0;i<numsSize;i++){
+        if(nums[i] == val){
+            for(int j=i;j<numsSize-1;j++){
+                nums[j] = nums[j+1];
+            }
+            numsSize--;
+            i--;
+        }
+    }
+    return numsSize;
+}

2b353b65f4de50a803cf4dbc8a9c808ca5e283db
Urvashi Zinzuvadiya <urvashi7211@gmail.com>
2026-09-09 17:59:41 +0530

    Time: 0 ms (100%), Space: 9.4 MB (57.89%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.c
@@ -0,0 +1,11 @@
+long long countCommas(long long n) {
+    long long ans = 0;
+    long long x = 1000;
+
+    while (x <= n) {
+        ans += n - x + 1;
+        x *= 1000;
+    }
+
+    return ans;
+}
